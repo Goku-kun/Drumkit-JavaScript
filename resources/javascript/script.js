bbc0fc4e96467fb95893b3dffe3b1a667b754f96
Goku-kun <jethvadharmarajsinh@gmail.com>
2020-12-18 09:03:43 +0530

    ADD: complete JavaScript drumkit

---
+++ resources/javascript/script.js
@@ -0,0 +1,26 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function playSoundPointer(e) {
+    const audio = document.querySelector(`audio[data-key="${e.path[1].dataset['key']}"]`)
+    const key = document.querySelector(`.key[data-key="${e.path[1].dataset['key']}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+document.addEventListener('keydown', playSound);
+document.addEventListener('pointerup', playSoundPointer);
